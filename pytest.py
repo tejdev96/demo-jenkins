aff608b1d49aba67a2a8d7445a9682954fc0c610
tejdev96 <128481779+tejdev96@users.noreply.github.com>
2024-05-28 14:03:32 +0530

    Update pytest.py

--- pytest.py
+++ pytest.py
@@ -1,6 +1,8 @@
 # 2. Write a Python program to get a string made of the first 2 and last 2 characters of a given string. 
 # If the string length is less than 2, return the empty string
 
+Make changes to the file so that the trigger works
+
 def get_first_last_chars(input_string):
 if len(input_string) < 2:
     return ""
